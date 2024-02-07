f4919df47160c037ebf38677d6a54724b7df9790
SeptoForce <septoforcemc@gmail.com>
2024-02-07 22:52:12 +0100

    Update colors and remove unnecessary code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -12,6 +12,21 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "special-red": "#ff453a",
+        "special-orange": "#ff9f0a",
+        "special-yellow": "#ffd60a",
+        "special-green": "#32d74b",
+        "special-teal": "#66d4cf",
+        "special-lightblue": "#6ac4dc",
+        "special-skyblue": "#5ac8f5",
+        "special-blue": "#0a84ff",
+        "special-indigo": "#5e5ce6",
+        "special-purple": "#bf5af2",
+        "special-pink": "#ff375f",
+        "special-brown": "#ac8e68",
+        "special-grey": "#98989d",
       },
     },
   },
